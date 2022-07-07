12fc2f0527aa34f04d742344745ded16b644bb26
Ernesto Alejandro Nappi <eanappi@gmail.com>
2022-07-07 14:52:21 -0300

    feat: add css LSP

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -52,4 +52,12 @@ require'lspconfig'.html.setup {
   capabilities = capabilities,
 }
 
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.cssls.setup {
+  capabilities = capabilities,
+}
+
 require'lspconfig'.tailwindcss.setup{}
